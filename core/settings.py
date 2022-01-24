d472f40cfd6f2ed2c50812d61769b16f94c4684c
Tynt-170 <10756022@ntub.edu.tw>
2022-01-24 13:42:14 +0800

    update (auth) : register page

--- core/settings.py
+++ core/settings.py
@@ -118,8 +118,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-urlpatterns = [
-    path('api/auth/', include('api.urls.auth_urls')),
-]
