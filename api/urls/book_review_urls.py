b938e3cf2f80f60e7a1ec5a75ccbd082154e057e
Tynt-170 <10756022@ntub.edu.tw>
2022-01-24 15:19:00 +0800

    feat (book) : add edit comment

--- api/urls/book_review_urls.py
+++ api/urls/book_review_urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from api.views.book_review_views import *
 
-
 app_name = 'book_review'
 
 urlpatterns = [
     path('add/', add_review, name='add'),
+    path('edit/<int:pk>/', edit_review, name='edit'),
 ]
