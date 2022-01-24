50a93f5ec75155ebd1a12053117f296f3b877222
Tynt-170 <10756022@ntub.edu.tw>
2022-01-24 16:52:45 +0800

    feat : update to heroku

--- core/settings.py
+++ core/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(bxma0%2x5#3o48lhgzvfo+^ssr8o#3nyc^q+t_4r(_$#%k*y_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'postgres://bfqkniyucuvaol:eddbc0215d1dceda16bc4d468a0712f5b58b8d4789bae2a063af4843f6d1899a@ec2-3-222-49-168.compute-1.amazonaws.com:5432/dilcbp3j0j6ru']
 
 # Application definition
 
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -113,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
